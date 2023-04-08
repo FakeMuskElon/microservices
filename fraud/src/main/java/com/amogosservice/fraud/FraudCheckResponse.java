b9216f79a8769095e3bef64026316c0c32a3db63
FakeMuskElon <fake.musk.elon@gmail.com>
2023-04-08 20:38:45 +0300

    fraud check added

---
+++ fraud/src/main/java/com/amogosservice/fraud/FraudCheckResponse.java
@@ -0,0 +1,4 @@
+package com.amogosservice.fraud;
+
+public record FraudCheckResponse(Boolean isFraudster) {
+}
---
+++ fraud/src/main/java/com/amogosservice/fraud/FraudCheckService.java
@@ -0,0 +1,23 @@
+package com.amogosservice.fraud;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+@AllArgsConstructor
+public class FraudCheckService {
+    private final FraudCheckHistoryRepository fraudCheckHistoryRepository;
+
+    public boolean isFroudulentCustomer(Integer customerId){
+        fraudCheckHistoryRepository.save(
+                FraudCheckHistory.builder()
+                        .customerId(customerId)
+                        .isFraudster(false)
+                        .createdAt(LocalDateTime.now())
+                        .build()
+        );
+        return false;
+    }
+}
